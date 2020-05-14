eabb1dd9aaa9a39f52ba96d2c9772eed684f6a7e
Steve <webz@swebster3369@gmail.com>
2020-05-14 18:22:16 +0100

    frontend setup

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  data(){
+    return {
+      title: ''
+    }
+  },
+  mounted(){
+    fetch('http://localhost:8085')
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        this.title = data;
+      });
+  }
+};
+</script>
