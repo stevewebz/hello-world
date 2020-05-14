2a361a23d724a9cad279ac68c458ec00869aa949
Steve <webz@swebster3369@gmail.com>
2020-05-14 17:48:30 +0100

    springboot-vuejs

---
+++ backend/src/main/java/com/gymfitness/backend/HomeController.java
@@ -0,0 +1,18 @@
+package com.gymfitness.backend;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@CrossOrigin
+public class HomeController {
+    @GetMapping(value="/")
+    public String home() {
+        return "Hello.";
+    }
+    
+}
---
+++ backend/src/test/java/com/gymfitness/backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymfitness.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
